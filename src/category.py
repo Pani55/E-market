90f9158f7834b131cd710d8fc17317dcb15ebfc8
Pani_55 <Dumbasss_rus@mail.ru>
2024-03-18 19:56:28 +0300

    few changes in category & product

--- src/category.py
+++ src/category.py
@@ -1,18 +1,43 @@
+from product import Product
+
+
 class Category:
     name: str
     description: str
-    goods: list
+    __goods: list
 
     count_of_categories = 0
     count_of_unique_goods = 0
 
     def __repr__(self):
-        return f"{self.name}, {self.description}, {self.goods}"
+        return f"Категория: {self.name}, Описание: {self.description}, Товары: {self.__goods}"
 
     def __init__(self, name: str, description: str, goods: list):
         self.name = name
         self.description = description
-        self.goods = goods
+        self.__goods = goods
 
         Category.count_of_categories += 1
-        Category.count_of_unique_goods += len(self.goods)
+        Category.count_of_unique_goods += len(self.__goods)
+
+    def add_goods(self, product):
+        if product not in self.__goods:
+            self.__goods.append(product)
+        return self.__goods
+
+    @property
+    def get_goods(self):
+        result = ''
+        for product in self.__goods:
+            result += f'{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n'
+
+        return result
+
+# prod_3 = Product.add_product('name3', 'descr3', 123, 10)
+# test_prod_2 = Product('name2', 'descr2', 50, 55)
+# test_prod = Product('name1', 'descr1', 10, 5)
+# test_cat = Category('test', 'test_descr', [test_prod])
+# print(test_cat.get_goods)
+# print(test_cat)
+# test_cat.add_goods(prod_3)
+# print(test_cat.get_goods)
--- src/product.py
+++ src/product.py
@@ -10,5 +10,22 @@ class Product:
     def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: float):
+        if new_price <= 0:
+            print("Введена некорректная цена")
+        else:
+            self.__price = new_price
+
+    @classmethod
+    def add_product(cls, name, descr, price, quantity):
+        object_ = Product(name, descr, price, quantity)
+
+        return object_
