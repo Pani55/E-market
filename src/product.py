e93b5f65717f532d4d45751db884c79eea6f59db
Pani_55 <Dumbasss_rus@mail.ru>
2024-03-21 19:57:35 +0300

    add __str__, fix __repr__

--- src/product.py
+++ src/product.py
@@ -5,7 +5,7 @@ class Product:
     quantity: int
 
     def __str__(self):
-        return f"{self.name}, {self.description}, {self.price}, {self.quantity}"
+        return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
 
     def __repr__(self):
         return f"{self.__class__.__name__}({self.name}, {self.description}, {self.__price}, {self.quantity}"
